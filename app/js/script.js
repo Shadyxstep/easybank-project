7fa4ae5c19502cb61baecd4e2dea80108d98dc9a
Leo Morgan <laumorgan.14@gmail.com>
2022-03-08 14:16:04 +0000

    added hamburger & overlay animation

--- app/js/script.js
+++ app/js/script.js
@@ -1,1 +1,21 @@
-console.log('hello world!');
+const btnHamburger = document.querySelector('#btnHamburger');
+const header = document.querySelector('.header');
+const overlay = document.querySelector('.overlay');
+
+
+btnHamburger.addEventListener('click', function(){
+    console.log('Ouch!');
+
+    if(header.classList.contains('open')){
+        // Close Hamburger Menu
+        header.classList.remove('open');
+        overlay.classList.add('fade-out');
+        overlay.classList.remove('fade-in');
+
+    } else {
+        //Open Hamburger Menu
+        header.classList.add('open');
+        overlay.classList.add('fade-in');
+        overlay.classList.remove('fade-out');
+    }
+});
