1f5ad53fafd6843e27887e79b0b4dcdd14c8ebb4
Leo Morgan <laumorgan.14@gmail.com>
2022-03-09 19:00:27 +0000

    finished mobile menu animation

--- app/js/script.js
+++ app/js/script.js
@@ -1,6 +1,9 @@
 const btnHamburger = document.querySelector('#btnHamburger');
+const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const overlay = document.querySelector('.overlay');
+const fadeElements = document.querySelectorAll('.has-fade');
+
 
 
 btnHamburger.addEventListener('click', function(){
@@ -9,13 +12,21 @@ btnHamburger.addEventListener('click', function(){
     if(header.classList.contains('open')){
         // Close Hamburger Menu
         header.classList.remove('open');
-        overlay.classList.add('fade-out');
-        overlay.classList.remove('fade-in');
+        body.classList.remove('noscroll');
+        fadeElements.forEach(function(element){
+            element.classList.remove('fade-in');
+            element.classList.add('fade-out');
+
+        });
 
     } else {
         //Open Hamburger Menu
+        body.classList.add('noscroll');
         header.classList.add('open');
-        overlay.classList.add('fade-in');
-        overlay.classList.remove('fade-out');
+        fadeElements.forEach(function(element){
+            element.classList.remove('fade-out');
+            element.classList.add('fade-in');
+
+        });
     }
 });
